2d0fce64fb0979ee629465ed2566060194b1bec7
Anas jaidi <anas.jaidi@icloud.com>
2024-03-12 14:22:21 +0000

    add componenet: #1

--- angular-basics-1/src/app/app.module.ts
+++ angular-basics-1/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParComponenet } from '../components/par/par.component';
+import { HeadComponent } from '../components/head/head.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ParComponenet, HeadComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-basics-1/src/components/par/par.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: './par.component.html',
+  styleUrl: './par.component.css',
+  selector: 'par-component'
+})
+export class ParComponenet {}
